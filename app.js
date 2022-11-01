5faab674533f4a6533f634aa10f3378a9aa97d20
tschmiedl <tschmiedl@me.com>
2022-11-01 16:58:28 -0700

    end of day one

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const aboutMeBtn = document.getElementById('aboutme')
+const aboutMeBtn = document.getElementById('aboutmebtn')
 
-const infoBoxDiv = document.getElementById('infobox')
+const aboutMeTextBox = document.getElementById('aboutmetext')
+aboutMeTextBox.style.display="none"
+
+const workExperienceBtn = document.getElementById('workexperiencebtn')
+
+const workExperienceTextBox = document.getElementById('workexperiencetext')
+workExperienceTextBox.style.display="none"
 
 
+// Button that opens up about me div 
 aboutMeBtn.addEventListener('click', () => {
-
+    aboutMeTextBox.style.display="block";
 }
 
 )
+workExperienceBtn.addEventListener('click', () => {
+    workExperienceTextBox.style.display="block";
+})
