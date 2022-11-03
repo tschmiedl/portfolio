df3da08f0d1fcfa3a77c19dcbacb1a8729044ec8
tschmiedl <tschmiedl@me.com>
2022-11-03 16:19:16 -0700

    Project finished

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// 
+// Pages function below
 const pages = document.getElementsByTagName('section')
 const buttons = document.getElementsByClassName('navbuttons')
 
 function onClickHandle(event){
     
     const pageId = event.target.innerText.toLowerCase()
-    console.log(pageId)
-
+    
 
 for (let i = 0; i < pages.length;i++){
     if (pageId === pages[i].id){
@@ -17,11 +16,23 @@ for (let i = 0; i < pages.length;i++){
     }
     
 }}
-
 for (let i=0; i < buttons.length; i++){
     buttons[i].addEventListener('click', onClickHandle)
 }
 
+// Used JQuery Background Image Carousel
+// Got this method from https://stackoverflow.com/questions/57548709/how-do-i-make-my-background-into-a-auto-slideshow-carousel
+$.backstretch([
+    "./images/bluegrey.jpeg",
+    "./images/brownhorse.jpeg",
+    "./images/green.jpeg",
+    "./images/green2.jpeg",
+    "./images/redboat.jpeg"
+  ], {
+      duration: 4000,
+      transition: 'fade|fade|fade|fade|fade',
+      transitionDuration: 400
+  });
 
 
 
